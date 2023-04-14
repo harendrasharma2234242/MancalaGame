44a3978a06092d73f7d91cdb347885e694b63fc3
alex31300 <2203556@swansea.ac.uk>
2023-04-14 16:54:23 +0100

    alex

--- src/main/java/com/mancala/mancalagame/BoardController.java
+++ src/main/java/com/mancala/mancalagame/BoardController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class BoardController {
     @FXML
@@ -70,6 +71,8 @@ public class BoardController {
     private Label mancalaLabel_P2;
     @FXML
     private final ArrayList<Hole> holes = new ArrayList<>();
+    private final ArrayList<Hole> holesPlayer1 = new ArrayList<>();
+    private final ArrayList<Hole> holesPlayer2 = new ArrayList<>();
     @FXML
     private final ArrayList<Mancala> mancalas = new ArrayList<>();
     private int holeNumber;
@@ -95,6 +98,10 @@ public class BoardController {
         for (int i = 0; i <= 2; i++) {
             mancalas.add(new Mancala(0));
             mancalaLabels.add("0");
+        }
+        for (int i = 0; i < 6; i++) {
+            holesPlayer1.add(holes.get(i));
+            holesPlayer2.add(holes.get(i+6));
         }
     }
     private void setCurrentPlayer() {
@@ -148,8 +155,6 @@ public class BoardController {
     }
     @FXML
     private void moveStones(int holeNumber) {
-        gameWon();
-
         int chosenHoleCount = holes.get(holeNumber).getCount();
         holes.get(holeNumber).setCount(0);
         holeLabels.set(holeNumber, "0");
@@ -212,11 +217,73 @@ public class BoardController {
             setCurrentPlayer();
             notification.setText("");
         }
+        gameEnd();
     }
 
-    private void gameWon() {
-        System.out.println("checked");
+    private void gameEnd() {
+        if (totalContents(holesPlayer1) == 0) {
+            System.out.println("p1 empty");
+            System.out.println("p1: " + totalContents(holesPlayer1));
+            System.out.println("p2: " + totalContents(holesPlayer2));
+            mancalas.get(1).setCount(mancalas.get(1).getCount() + totalContents(holesPlayer2));
+            mancalaLabels.set(1, String.valueOf(mancalas.get(1).getCount()));
+            for (Hole h : holes) {
+                h.setCount(0);
+            }
+            Collections.fill(holeLabels, "0");
+            setLabels();
+            getWinner();
+        } else if (totalContents(holesPlayer2) == 0) {
+            System.out.println("p2 empty");
+            System.out.println("p1: " + totalContents(holesPlayer1));
+            System.out.println("p2: " + totalContents(holesPlayer2));
+            mancalas.get(0).setCount(mancalas.get(0).getCount() + totalContents(holesPlayer1));
+            mancalaLabels.set(0, String.valueOf(mancalas.get(0).getCount()));
+            for (Hole h : holes) {
+                h.setCount(0);
+            }
+            Collections.fill(holeLabels, "0");
+            setLabels();
+            getWinner();
+        }
+    }
 
+    private int getWinner() {
+        int score1 = mancalas.get(0).getCount();
+        int score2 = mancalas.get(1).getCount();
+        int winner;
+        if (score1 < score2) {
+            notification.setText("Player 2 wins!");
+            winner = 2;
+        } else if (score1 > score2) {
+            notification.setText("Player 1 wins!");
+            winner = 1;
+        } else {
+            notification.setText("It's a draw!");
+            winner = -1;
+        }
+        button0.setDisable(true);
+        button1.setDisable(true);
+        button2.setDisable(true);
+        button3.setDisable(true);
+        button4.setDisable(true);
+        button5.setDisable(true);
+        button6.setDisable(true);
+        button7.setDisable(true);
+        button8.setDisable(true);
+        button9.setDisable(true);
+        button10.setDisable(true);
+        button11.setDisable(true);
+
+        return winner;
+    }
+
+    private int totalContents(ArrayList<Hole> holes) {
+        int total = 0;
+        for (Hole h : holes) {
+            total += h.getCount();
+        }
+        return total;
     }
     private boolean checkEmpty(int holeNumber) {
         return holes.get(holeNumber).getCount() != 0;
