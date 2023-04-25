e478986c591e8eb1e7dc8b7a95d07086a68b51a6
cloudt9 <2222520@swansea.ac.uk>
2023-04-25 23:16:01 +0100

    playermode

--- src/main/java/com/mancala/mancalagame/query/QueryUtility.java
+++ src/main/java/com/mancala/mancalagame/query/QueryUtility.java
@@ -1,9 +1,9 @@
 package com.mancala.mancalagame.query;
 
 public class QueryUtility {
-    private static  final String DBURL = "jdbc:mysql://localhost:3306/mancala";
-    private static final String DBNAME = "sparrow";
-    private static final String PASS = "Bigdodo";
+    private static  final String DBURL = "jdbc:mysql://localhost:3306/sparrow";
+    private static final String DBNAME = "root";
+    private static final String PASS = "bigdodo";
     private static final String LOGIN_QUERY = "SELECT password, is_Active, is_admin from user where username= ? AND is_admin = 0";
     private  static final String ADMIN_LOGIN_QUERY = "SELECT password from user where username= ? AND is_admin = 1";
     private String  SIGNUP = "INSERT INTO user (username, password, name) VALUES (?,?, ?)";
